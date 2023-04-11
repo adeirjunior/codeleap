95550ddb0a02827ffe87cec04854a1455a6c0842
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-04-10 21:06:47 -0400

    auto time diff update

--- src/components/dom/Form.tsx
+++ src/components/dom/Form.tsx
@@ -6,11 +6,11 @@ export default function Form() {
         <h2 className="text-xl font-bold">What's on your mind?</h2>
         <div className="flex flex-col mt-4">
           <label className="text-base font-medium mb-1" htmlFor="title">Title</label>
-          <input className="border-[#999999] border rounded-md" title="title" id="title" type="text" />
+          <input placeholder="Hello world" className="border-[#999999] border rounded-md px-2 py-1 text-sm" title="title" id="title" type="text" />
         </div>
         <div className="flex flex-col mt-4">
           <label className="text-base font-medium mb-1" htmlFor="content">Content</label>
-          <textarea className="border-[#999999] border rounded-md px-2" title="content" id="content" rows={10}></textarea>
+          <textarea placeholder="Content here" className="border-[#999999] border rounded-md px-2 py-1 text-sm resize-none" title="content" id="content" rows={4}></textarea>
         </div>
         <button className="bg-primary-color text-white font-bold px-8 py-1 rounded-lg mt-6 float-right" type="button">Create</button>
       </div>
--- src/components/dom/PostCard.tsx
+++ src/components/dom/PostCard.tsx
@@ -2,7 +2,7 @@ import Delete from '../svgs/Delete'
 import Edit from '../svgs/Edit'
 import { CareerCardProp, Career } from '../../interfaces'
 import { useDispatch } from "react-redux"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { deleteCareer } from '../../actions'
 import { AppDispatch } from '../../redux/store'
 
@@ -10,44 +10,54 @@ import { AppDispatch } from '../../redux/store'
 export default function CareerCard({ data }: CareerCardProp) {
   const { id, title, content, created_datetime, username }: Career = data
   const dispatch: AppDispatch = useDispatch()
-  const dateNow = new Date(created_datetime)
+  const dateOld = new Date(created_datetime)
+  const [now, setNow] = useState(new Date())
 
   const handleClick = (id: number) => {
     dispatch(deleteCareer(id))
   }
 
   function diffHour(timeAgo: Date): number {
-    const agora: Date = new Date();
-    const diferenca: number = agora.getTime() - timeAgo.getTime();
-    console.log(diferenca)
-    return diferenca;
+    const agora: Date = new Date()
+    const diferenca: number = agora.getTime() - timeAgo.getTime()
+    return diferenca
   }
 
-  function timeAgo(dateNow: Date): string {
-    const diferenca: number = diffHour(dateNow);
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setNow(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [])
+
+  function timeAgo(dateOld: Date): string {
+    const diferenca: number = diffHour(dateOld)
   
-    const seconds: number = Math.floor(diferenca / 1000);
-    const minutes: number = Math.floor(seconds / 60);
-    const hours: number = Math.floor(minutes / 60);
-    const days: number = Math.floor(hours / 24);
-    const weeks: number = Math.floor(days / 7);
-    const months: number = Math.floor(weeks / 4.34524);
-    const year: number = Math.floor(months / 12);
+    const seconds: number = Math.floor(diferenca / 1000)
+    const minutes: number = Math.floor(seconds / 60)
+    const hours: number = Math.floor(minutes / 60)
+    const days: number = Math.floor(hours / 24)
+    const weeks: number = Math.floor(days / 7)
+    const months: number = Math.floor(weeks / 4.34524)
+    const year: number = Math.floor(months / 12)
   
     if (year > 0) {
-      return year === 1 ? "1 year ago" : year + " years ago";
+      return year === 1 ? "1 year ago" : year + " years ago"
     } else if (months > 0) {
-      return months === 1 ? "1 month ago" : months + " months ago";
+      return months === 1 ? "1 month ago" : months + " months ago"
     } else if (weeks > 0) {
-      return weeks === 1 ? "1 week ago" : weeks + " weeks ago";
+      return weeks === 1 ? "1 week ago" : weeks + " weeks ago"
     } else if (days > 0) {
-      return days === 1 ? "1 day ago" : days + " days ago";
+      return days === 1 ? "1 day ago" : days + " days ago"
     } else if (hours > 0) {
-      return hours === 1 ? "1 hour ago" : hours + " hours ago";
+      return hours === 1 ? "1 hour ago" : hours + " hours ago"
     } else if (minutes >= 1) {
-      return minutes === 1 ? "1 minute ago" : minutes + " minutes ago";
+      return minutes === 1 ? "1 minute ago" : minutes + " minutes ago"
     } else {
-      return "some seconds ago";
+      return "some seconds ago"
     }
   }
   
@@ -72,7 +82,7 @@ export default function CareerCard({ data }: CareerCardProp) {
       <div className="p-4">
         <div className="flex justify-between items-center mb-1">
           <p className="text-[#777777] font-bold">{`@${username}`}</p>
-          <p className="text-[#777777] ">{timeAgo(dateNow)}</p>
+          <p className="text-[#777777] ">{timeAgo(dateOld)}</p>
         </div>
         <p>{content}</p>
       </div>
