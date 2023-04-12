61e86f7a3f3a396bceb3020a50440a02b4648371
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-04-11 20:29:52 -0400

    preventDefault added

--- src/components/dom/Form.tsx
+++ src/components/dom/Form.tsx
@@ -5,22 +5,66 @@ import { AppDispatch, RootState } from "../../redux/store";
 import { addCareer } from "../../actions";
 
 export default function Form() {
-  const { user: { name } } = useSelector((state: RootState) => state.user)
-  const [career, setCareer] = useState<Career>({content: '', title: '', username: name, created_datetime: null, id: null})
-  const dispatch: AppDispatch = useDispatch()
+  const {
+    user: { name },
+  } = useSelector((state: RootState) => state.user);
+  const [career, setCareer] = useState<Career>({
+    content: "",
+    title: "",
+    username: name,
+    created_datetime: null,
+    id: null,
+  });
+  const dispatch: AppDispatch = useDispatch();
 
-    return (
-      <div className="border-[#999999] border rounded-2xl overflow-hidden mb-5 p-5">
-        <h2 className="text-xl font-bold">What's on your mind?</h2>
+  const submit = (e) => {
+    e.preventDefault()
+    dispatch(addCareer(career));
+  };
+
+  return (
+    <div className="border-[#999999] border rounded-2xl overflow-hidden mb-5 p-5">
+      <h2 className="text-xl font-bold">What's on your mind?</h2>
+      <form onSubmit={submit}>
         <div className="flex flex-col mt-4">
-          <label className="text-base font-medium mb-1" htmlFor="title">Title</label>
-          <input onChange={event => setCareer({...career, title: event.target.value})} placeholder="Hello world" className="border-[#999999] border rounded-md px-2 py-1 text-sm" title="title" id="title" type="text" />
+          <label className="text-base font-medium mb-1" htmlFor="title">
+            Title
+          </label>
+          <input
+            required
+            onChange={(event) =>
+              setCareer({ ...career, title: event.target.value })
+            }
+            placeholder="Hello world"
+            className="border-[#999999] border rounded-md px-2 py-1 text-sm"
+            title="title"
+            id="title"
+            type="text"
+          />
         </div>
         <div className="flex flex-col mt-4">
-          <label className="text-base font-medium mb-1" htmlFor="content">Content</label>
-          <textarea onChange={event => setCareer({...career, content: event.target.value})} placeholder="Content here" className="border-[#999999] border rounded-md px-2 py-1 text-sm resize-none" title="content" id="content" rows={4}></textarea>
+          <label className="text-base font-medium mb-1" htmlFor="content">
+            Content
+          </label>
+          <textarea
+            required
+            onChange={(event) =>
+              setCareer({ ...career, content: event.target.value })
+            }
+            placeholder="Content here"
+            className="border-[#999999] border rounded-md px-2 py-1 text-sm resize-none"
+            title="content"
+            id="content"
+            rows={4}
+          ></textarea>
         </div>
-        <button onClick={() => dispatch(addCareer(career))} className="bg-primary-color text-white font-bold px-8 py-1 rounded-lg mt-6 float-right" type="button">Create</button>
-      </div>
-    )
-  }
+        <button
+          className="bg-primary-color text-white font-bold px-8 py-1 rounded-lg mt-6 float-right"
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,39 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import { AppDispatch, RootState } from "../redux/store"
-import { useNavigate } from "react-router-dom"
-import { addUser } from "../actions"
-import { User } from '../interfaces'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../actions";
+import { User } from "../interfaces";
 
 function Signup() {
-  const dispatch: AppDispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch();
   const [inputText, setInputText] = useState<User>();
-  const navigate = useNavigate()
-  const { user } = useSelector((state: RootState) => state.user)
+  const navigate = useNavigate();
+  const { user } = useSelector((state: RootState) => state.user);
 
   const submit = () => {
-    dispatch(addUser(inputText))
-  }
+    dispatch(addUser(inputText));
+  };
 
   useEffect(() => {
-    if (user.name !== '') navigate('/')
-  },[submit])
+    if (user.name !== "") navigate("/");
+  }, [submit]);
 
   return (
-    <div className='fixed flex justify-center items-center top-0 left-0 w-full h-full bg-gray-light'>
-      <div className='bg-white flex items-center w-full max-w-[500px] flex-col p-6 rounded-xl border border-[#CCCCCC]'>
-        
-        <div className='flex flex-col w-full'>
-        <h2 className='text-xl font-bold mb-5'>Welcome to CodeLeap network!</h2>
-        <label className='mb-1' htmlFor="username">Please enter your username</label>
-        <input onChange={e => setInputText({name: e.target.value})} placeholder='John doe' className='border border-[#777777] mb-4 rounded-lg px-3 py-1' type="text" id="username" />
-        </div>
-        <div className='flex items-end flex-col w-full'>
-        <button onClick={submit} className='float-right bg-primary-color font-bold uppercase text-white w-auto py-1 rounded-lg min-w-[111px]' type='button'>Enter</button>
-        </div>
+    <div className="fixed flex justify-center items-center top-0 left-0 w-full h-full bg-gray-light">
+      <div className="bg-white flex items-center w-full max-w-[500px] flex-col p-6 rounded-xl border border-[#CCCCCC]">
+        <form className="w-full" onSubmit={submit}>
+          <div className="flex flex-col w-full">
+            <h2 className="text-xl font-bold mb-5">
+              Welcome to CodeLeap network!
+            </h2>
+            <label className="mb-1" htmlFor="username">
+              Please enter your username
+            </label>
+            <input
+              onChange={(e) => setInputText({ name: e.target.value })}
+              placeholder="John doe"
+              className="border border-[#777777] mb-4 rounded-lg px-3 py-1"
+              type="text"
+              id="username"
+              required
+            />
+          </div>
+          <div className="flex items-end flex-col w-full">
+            <button
+              className="float-right bg-primary-color font-bold uppercase text-white w-auto py-1 rounded-lg min-w-[111px]"
+              type="submit"
+            >
+              Enter
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
