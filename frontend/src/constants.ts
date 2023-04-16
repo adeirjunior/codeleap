302d338a25201a5fa95823f2dcc193b853f74dd8
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-04-15 21:11:35 -0400

    changed backend url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000/api/careers/"
-    : "https://codeleap-backend.vercel.app/api/careers/";
+    : "https://codeleap-pi.vercel.app/api/careers/";
